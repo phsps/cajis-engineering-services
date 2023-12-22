1025d9e68cfffe24c679d615719cd94d330be91f
Ugadu Mercy <earlsamm@gmail.com>
2023-12-22 05:56:10 +0100

    Changing the index.html files to .php with modification adding header and footer files for easier editing

---
+++ front_footer.php
@@ -0,0 +1,195 @@
+
+    <footer>
+        <div class="row container foot">
+          <div class="col imgtext">
+            <img src="assets/images/CAJIS-ICON-white.png" alt="" class="logo" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">
+            <p data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000">
+              The growth and successes of the company are attributable to the company’s formidable team and their vision to build edifices that are unquestionably safe, aesthetically appealing and endlessly functional.         
+            </p>
+            <div class="socials">
+              <li data-aos="fade-right" data-aos-delay="100" data-aos-duration="1000"><a href="#"><i class="bi bi-facebook"></i></a></li>
+              <li data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000"><a href="#"><i class="bi bi-instagram"></i></a></li>
+              <li data-aos="fade-left" data-aos-delay="100" data-aos-duration="1000"><a href="#"><i class="bi bi-twitter"></i></a></li>
+            </div>
+          </div>
+          <div class="col">
+            <h3 data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">
+              Quick Links
+              <div class="underline"><span></span></div>
+            </h3>
+            <ul>
+              <li data-aos="fade-left" data-aos-delay="100" data-aos-duration="1000"><a href="about.html">About</a></li>
+              <li data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000"><a href="project.html">Projects.</a></li>
+              <li data-aos="fade-right" data-aos-delay="100" data-aos-duration="1000"><a href="services.html">Services</a></li>
+              <li data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000"><a href="dredging.html">Ecko Dredging</a></li>
+              <li data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000"><a href="logistics.html">Blut Logistics</a></li>
+            </ul>
+          </div>
+          <div class="col">
+            <h3 data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">
+              Contacts
+              <div class="underline"><span></span></div>
+            </h3>
+            <ul>
+              <li data-aos="fade-right" data-aos-delay="100" data-aos-duration="1000">
+                <i class="bi bi-house-fill house"></i>C20 Nicon Town  Lekki, Lagos state 
+              </li>
+              <li data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000">
+                <a href="mailto:info@cajisengineeringservices.com"
+                  >
+                  <i class="bi bi-envelope-fill email"></i> info@cajisengineeringservices.com</a
+                >
+              </li>
+              <li data-aos="fade-left" data-aos-delay="100" data-aos-duration="1000">
+                <a href="tel:+234 803 749 2692"
+                  ><i class="bi bi-telephone-fill telephone"></i>+234 803 749 2692</a
+                >
+              </li>
+            </ul>
+          </div>
+
+          <div class="col">
+            <h3 data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">
+              Company
+              <div class="underline"><span></span></div>
+            </h3>
+            <ul>
+                <li data-aos="fade-left" data-aos-delay="100" data-aos-duration="1000"><a href="services.html">Our Services</a></li>
+                <li data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000"><a href="#">Privacy Policy</a></li>
+                <li data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000"><a href="#">Faqs</a></li>
+              </ul>
+          
+        </div>
+          </div>
+        </div>
+        <div class="line"></div>
+       
+      
+            <p class="phsps">
+                &copy; 2023 CAJIS - By <a href="https://phsps.com">Phsps Apps</a>
+              </p>
+
+      </footer>
+
+
+
+
+
+
+
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+<script>
+    $('.blog_slider_area').owlCarousel({
+      autoplay:true,
+      slideSpeed:10000,
+      items:1,
+      loop:true,
+      nav:false,
+      margin:30,
+      dots:false,
+      responsive:{
+        320:{
+          items:1
+        },
+        767:{
+          items:2
+        },
+        600:{
+          items:2
+        },
+        1000:{
+          items:2
+        }
+      }
+    });
+  </script>
+
+
+<script>
+  $('.blog_slider_area_2').owlCarousel({
+    // autoplay:true,
+    // slideSpeed:10000,
+    items:1,
+    nav:false,
+    margin:30,
+    dots:false,
+    responsive:{
+      320:{
+        items:1
+      },
+      767:{
+        items:2
+      },
+      600:{
+        items:2
+      },
+      1000:{
+        items:1
+      }
+    }
+  });
+</script>
+
+
+<script>
+  $(".slides").owlCarousel({
+    autoplay: true,
+    slideSpeed: 10000,
+    items: 1,
+    loop: true,
+    nav: false,
+    margin: 30,
+    dots: false,
+    responsive: {
+      320: {
+        items: 1,
+      },
+      767: {
+        items: 2,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+</script>
+
+
+<script>
+  const swiper = new Swiper('.swiper', {
+  autoHeight: true,
+  loop: true,
+  autoplay: true,
+
+  // If we need pagination
+  pagination: {
+      el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+
+  });
+</script>
+  
+
+
+      <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+      <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+      <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+      AOS.init({
+        once: false
+      });
+    </script>
+</body>
+</html>
---
+++ front_header.php
@@ -0,0 +1,64 @@
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CAJIS | Constructon Company</title>
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js" defer></script>
+      <!-- <script src="https://unpkg.com/swiper/swiper-bundle.min.js" defer></script> -->
+      <script src="assets/js/script.js" defer></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <link href="/your-path-to-uicons/css/uicons-[your-style].css" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
+    <link rel="shortcut icon" href="assets/images/CAJIS-favicon.png" type="image/x-icon">
+    <!-- <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> -->
+     <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+
+
+      <!--====== HEADER =======-->
+      <header class="header" id="header">
+        <nav class="nav container">
+            <a href="index.html" class="nav_logo">
+                <img src="assets/images/CAJIS-ICON.png" alt="">
+            </a>
+
+            <div class="nav_menu" id="nav-menu">
+                <ul class="nav_list">
+                    <li class="nav_item">
+                        <a href="index.html" class="nav_link">Home</a>
+                    </li>
+
+                    <li class="nav_item">
+                        <a href="about.html" class="nav_link">About</a>
+                    </li>
+
+                    <li class="nav_item">
+                        <a href="services.html" class="nav_link">Services</a>
+                    </li>
+
+                    <li class="nav_item">
+                        <a href="project.html" class="nav_link">Projects</a>
+                    </li>
+
+                    <li class="nav_item">
+                        <a href="contact.html" class="nav_link">Contact</a>
+                    </li>
+                </ul>
+
+
+                 <!-----Close button-->
+                 <div class="nav_close" id="nav-close">
+                    <i class="bi bi-x"></i>
+                 </div>
+            </div>
+
+            <!-----Toggle button------>
+            <div class="nav_toggle" id="nav-toggle">
+                <i class="bi bi-list"></i>
+            </div>
+        </nav>
+     </header>
